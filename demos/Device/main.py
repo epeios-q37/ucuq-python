c4636e1d882852e9c77c5318a85f763ec8f2e2b2
epeios-q37 <epeios-q37@users.noreply.github.com>
2024-12-25 15:17:16 +0100

    25/12/2024 15:17:16

---
+++ demos/Device/main.py
@@ -0,0 +1,23 @@
+import os, sys
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.extend(("../..","../../atlastk.zip"))
+
+import atlastk, ucuq, json
+
+def acConnect(dom):
+  infos = ucuq.ATKConnect(dom, BODY)
+  dom.setValue("Infos", json.dumps(infos, indent=2))
+
+CALLBACKS = {
+  "": acConnect
+}
+
+with open('Body.html', 'r') as file:
+  BODY = file.read()
+
+with open('Head.html', 'r') as file:
+  HEAD = file.read()
+
+atlastk.launch(CALLBACKS, headContent=HEAD)
+
