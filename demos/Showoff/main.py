e6521228dd3e5dc78fcdff6270fed8284983d1d9
epeios-q37 <epeios-q37@users.noreply.github.com>
2026-04-24 17:04:46 +0200

    24/04/2026 17:04:46

--- demos/Showoff/main.py
+++ demos/Showoff/main.py
@@ -22,20 +22,36 @@ DEVICES_ = ("Alfa", "India", "Lima", "Golf")
 
 HTML_OPTION_ = "<option>{}</option>"
 
+
+def combinaisons_(A):
+  R = []
+  n = len(A)
+  for r in range(1, n + 1):
+    def rec(start, prefix):
+      if len(prefix) == r:
+        R.append(" ".join(prefix))
+        return
+      for i in range(start, n):
+        rec(i + 1, prefix + [A[i]])
+
+    rec(0, [])
+  return R
+
+
 def atk(dom):
-  devices = "\n".join(HTML_OPTION_.format(device) for device in DEVICES_)
-    
+  devices = "\n".join(HTML_OPTION_.format(device) for device in combinaisons_(DEVICES_))
+  
   dom.inner("", BODY.format(devices, DEVICES_[0], *DEVICES_[:3]))  # type: ignore # noqa: F821
   colors.fill(dom)
   colors.update(dom)
   dom.executeVoid("handleClearable();toggleFieldsetByLegend('Showoff', false);")
-  
-  
+
+
 def atkPartnerConnect(dom):
   partner.connect(dom.getValue("PartnerDevice"))
   dom.executeVoid("toggleFieldsetByLegend('Partner', true)")
-  
-  
+
+
 def atkPartnerBuzzer(dom):
   partner.Buzzer()
 
@@ -43,18 +59,18 @@ def atkPartnerBuzzer(dom):
 def atkPartnerOLED(dom):
   partner.OLED()
 
-  
+
 def atkPartnerRing(dom):
   partner.Ring()
 
-  
+
 def atkPartnerLCD(dom):
   partner.LCD()
 
 
 def atkPartnerListen(dom):
   partner.Listen(dom)
-  
+
 
 # Called by JS 'partnerListen' script (itself launched by above 'atkPartnerListen' callback)
 def atkPartnerDisplaySpokenColor(dom):
@@ -64,12 +80,12 @@ def atkPartnerDisplaySpokenColor(dom):
 def atkPartnerIndy(dom):
   partner.indy()
 
-  
+
 def atkShowConnect(dom):
   show.connect(tuple(dom.getValues(("ShowLeftDevice", "ShowMiddleDevice", "ShowRightDevice")).values()))
   dom.executeVoid("toggleFieldsetByLegend('Show', true)")
-  
-  
+
+
 def syncTest():
   timestamp = time.time() + 1.5
   
@@ -80,25 +96,25 @@ def syncTest():
   show.sleepUntil(timestamp + 1)
   
   show.devices.rings.flash()
-  
-  
+
+
 def atkShowSync(dom):
   ucuq.ntpSetTime()
   syncTest()
-  
+
 def atkShowTest():
   syncTest()  
-  
+
 def atkShowIndy(dom):
   timestamp = show.countdownIfSelected(dom, time.time() + DELAY)
   indy.launch(timestamp)
-  
+
 
 def atkShowPink(dom):
   timestamp = show.countdownIfSelected(dom, time.time() + DELAY)
   pink.launch(timestamp)
-  
-  
+
+
 def atkShowFugue(dom):
   timestamp = show.countdownIfSelected(dom, time.time() + DELAY)
   trio.launch(timestamp)
@@ -112,13 +128,13 @@ def atkShowColorOnce(dom):
   scheme, delay = dom.getValues((colors.W_SCHEMES, colors.W_DELAY)).values()
   timestamp = show.countdownIfSelected(dom, time.time() + DELAY)
   colors.launch(int(scheme), timestamp, float(delay), 1)
-  
-  
+
+
 def atkShowColorRepeat(dom):
   scheme, delay, repeat = dom.getValues((colors.W_SCHEMES, colors.W_DELAY, colors.W_REPEAT)).values()
   timestamp = show.countdownIfSelected(dom, time.time() + DELAY)
   colors.launch(int(scheme), timestamp, float(delay), int(repeat))
-  
+
 if os.environ.get("PREFIX", "").startswith("/data/data/com.termux"):
   atlastk.set_supplier(lambda url: os.system(f'am start -n com.android.chrome/com.google.android.apps.chrome.Main -d "{url}"'))  
 
